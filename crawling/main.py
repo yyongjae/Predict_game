e9c1254b6cd7e93b3571b273ca1c8eb7050a61d9
Cho Yong Jae <dydwo706@naver.com>
2021-11-09 23:46:10 +0900

    경고, 스로잉, 퇴장 예외 처리.
    경고, 스로잉 값이 없으면 0으로 처리, 퇴장 값이 있으면 pop

--- crawling/main.py
+++ crawling/main.py
@@ -1,12 +1,18 @@
+
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import csv
 #<죵재>
 #/Users/yongcho/Downloads/chromedriver
+#<aodtns>
 #C:\PythonPractice\Python\Web Page Scraping\chromedriver.exe
+yong_path = "/Users/yongcho/Desktop/Tool/chromedriver"
+maeng_path = "C:\PythonPractice\Python\Web Page Scraping\chromedriver.exe"
 
-browser = webdriver.Chrome("C:\PythonPractice\Python\Web Page Scraping\chromedriver.exe") #'./chromedriver'
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~driver 수정~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+browser = webdriver.Chrome(yong_path) #'./chromedriver'
 # browser.maximize_window() #창 최대화
 url = 'https://www.flashscore.co.kr/soccer/england/premier-league/archive/'
 browser.get(url)
@@ -32,25 +38,27 @@ year1 = 2020
 year2 = 2021
 game_cnt = 0
 season_data = [['' for x in range(36)] for _ in range(1520)] #모든 데이터를 1차원 배열로 담을거임
-for i in range(4):
+for i in range(1,4):
     year1 -= i
     year2 -= i
     browser.find_element_by_link_text(f"프리미어리그 {year1}/{year2}").click()
-
+    browser.implicitly_wait(5)
     #시즌 페이지에서 결과창 불러오기
     browser.execute_script("window.scrollTo(0,300)") #300크기만큼 스크롤 내림
     browser.find_element_by_link_text("결과").click()
-
+    browser.implicitly_wait(5)
 
     #결과 창에서 id 찾기 + 더 많은 경기 보기
     soup = BeautifulSoup(browser.page_source,'lxml')
     for j in range(3):
+        
         browser.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+        browser.implicitly_wait(3)
         more_match = soup.find("a",attrs={"class":"event__more event__more--static"}).get_text()
         if len(more_match) == 0:
             break
         browser.find_element_by_link_text("더 많은 경기 보기").click()
-        time.sleep(1)
+        browser.implicitly_wait(5)
 
     soup = BeautifulSoup(browser.page_source, 'lxml')
     arr = soup.find_all('div', attrs={'title':"경기세부사항을 보려면 클릭"})
@@ -61,16 +69,17 @@ for i in range(4):
         ar['id'] = ar['id'][4:]
         id_arr.append(ar['id'])
 
-    for j in id_arr:
+    for j in range(len(id_arr)):
         #새로운 창에 대해 url 얻기
-        browser2 = webdriver.Chrome("C:\PythonPractice\Python\Web Page Scraping\chromedriver.exe") #'./chromedriver'
-        url_for_statistic = f"https://www.flashscore.co.kr/match/{j}/#match-summary/match-statistics/0"
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~driver 수정~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        browser2 = webdriver.Chrome(yong_path) #'./chromedriver'
+        url_for_statistic = f"https://www.flashscore.co.kr/match/{id_arr[j]}/#match-summary/match-statistics/0"
         browser2.get(url_for_statistic)
         soup2 = BeautifulSoup(browser2.page_source, 'lxml')
 
 
         #팀 이름 가져오기
-        match = soup2.find_all("a",attrs={"class":"participantName___3lRDM1i overflow___cyQxKBr"})
+        match = soup2.find_all("a",attrs={"class":"participant__participantName participant__overflow"})
         name_list = []
         for m in match:
             m_strip = str(m['href']).split('/')
@@ -80,10 +89,9 @@ for i in range(4):
 
 
         #점수로 승, 무, 패 결정하기
-        score = soup2.find("div",attrs={"class":"wrapper___3rU3Jah"})
-        score_list = str(score).split("<span>") #두 번째 데이터 첫 번째 문자, 세 번째 데이터 첫 번째 문자가 점수
-        home_score = score_list[1][0]
-        away_score = score_list[2][0]
+        score = soup2.find("div",attrs={"class":"detailScore__wrapper"}).get_text()
+        home_score = score[0]
+        away_score = score[2]
         if home_score > away_score:
             Win_Draw_Lose_HomeTeam = 2
         elif home_score == away_score:
@@ -93,9 +101,10 @@ for i in range(4):
 
 
         #통계 데이터 가져오기
-        category = soup2.find_all("div",attrs={"class":"categoryName___3Keq6yi"})
-        home_data = soup2.find_all("div", attrs={"class":"homeValue___Al8xBea"})
-        away_data = soup2.find_all("div", attrs={"class":"awayValue___SXUUfSH"})
+
+        category = soup2.find_all("div",attrs={"class":"statCategoryName"})
+        home_data = soup2.find_all("div", attrs={"class":"statHomeValue"})
+        away_data = soup2.find_all("div", attrs={"class":"statAwayValue"})
         home_data_score = []
         away_data_score = []
         category_name = []
@@ -105,11 +114,9 @@ for i in range(4):
             away_data_score.append(a.get_text())
         for c in category:
             category_name.append(c.get_text())
-
         # for k in range(len(category_name)):
         #     print(f"홈팀 {category_name[k]} : {home_data_score[k]}")
         #     print(f"어웨이팀 {category_name[k]} : {away_data_score[k]}")
-
         #print(home_name, away_name, home_score, away_score, home_data_score[:], away_data_score[:])
         #exit(0)
 
@@ -117,6 +124,17 @@ for i in range(4):
         season_data[game_cnt][1] = away_name
         season_data[game_cnt][2] = home_score
         season_data[game_cnt][3] = away_score
+        
+        if '경고' not in category_name:
+            home_data_score.insert(8,0)
+            away_data_score.insert(8,0)
+        if '스로우인' not in category_name:
+            home_data_score.insert(11,0)
+            away_data_score.insert(11,0)
+        if '퇴장' in category_name:
+            home_data_score.pop(category_name.index('퇴장'))
+            away_data_score.pop(category_name.index('퇴장'))
+            
         for k in range(4, 36):
             if k % 2 == 0:
                 season_data[game_cnt][k] = home_data_score[(k//2)-2]
@@ -128,3 +146,4 @@ for i in range(4):
         game_cnt += 1
 
         browser2.close()
+    browser.back()
